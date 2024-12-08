82bc25eef68b06e694d43ca0461d8eb593f8fd4d
NexGenITLtd <nexgenitltd@gmail.com>
2024-12-08 12:32:06 +0600

    Sms setting update

--- database/seeders/SmsSettingsSeeder.php
+++ database/seeders/SmsSettingsSeeder.php
@@ -15,12 +15,12 @@ class SmsSettingsSeeder extends Seeder
     public function run(): void
     {
         SmsSetting::create([
-            'api_key' => 'example_api_key',
-            'sender_id' => 'example_sender',
-            'user_email' => 'admin@example.com',
+            'api_key' => '181785150221847320241208121128pmGpW9kUMn',
+            'sender_id' => '411',
+            'user_email' => 'ashikurashik.sc@gmail.com',
             'store_id' => 1,
             'balance' => 100.00,
-            'sms_rate' => 0.5,
+            'sms_rate' => 0.40,
             'sms_count' => 0,
             'api_url'    => 'https://24bulksms.com/24bulksms/api/api-sms-send',
         ]);
